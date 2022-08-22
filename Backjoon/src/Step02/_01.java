e9f36b8d95029f95a646cab90f74444d1e82b5a8
lazca2080 <lazca2080@gmail.com>
2022-08-22 18:04:02 +0900

    수정

--- Backjoon/src/Step02/_01.java
+++ Backjoon/src/Step02/_01.java
@@ -1,5 +1,25 @@
 package Step02;
 
-public class _01 {
+import java.util.Scanner;
 
+/*
+ * 날짜 : 2022/08/22
+ * 이름 : 박종협
+ * 내용 : 백준 2단계 1번 : 두 수 비교하기
+ */
+public class _01 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a > b) {
+			System.out.println(">");
+		}else if(a < b) {
+				System.out.println("<");
+		}else {
+				System.out.println("==");
+		}
+	}
 }
