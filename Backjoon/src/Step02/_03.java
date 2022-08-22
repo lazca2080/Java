270f7d33d1f65c02e3d6a932b87560d658d13f79
lazca2080 <lazca2080@gmail.com>
2022-08-22 19:45:57 +0900

    수정

--- Backjoon/src/Step02/_03.java
+++ Backjoon/src/Step02/_03.java
@@ -1,10 +1,24 @@
 package Step02;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/08/22
  * 이름 : 박종협
- * 내용 : 백준 2단계 2번 : 시험 성적
+ * 내용 : 백준 2단계 3번 : 윤년
  */
 public class _03 {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			int a = sc.nextInt();
+			if(a%4==0) {
+				if(a%400==0 || a%100!=0) {
+					System.out.println("1");
+				}else {
+					System.out.println("0");
+				}
+			}else {
+					System.out.println("0");
+			}
+	}
 }
