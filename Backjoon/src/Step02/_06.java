5b74b77ec6ca80abe8a69240ebd4b8fd918754b6
lazca2080 <lazca2080@gmail.com>
2022-08-23 19:52:43 +0900

    0823

--- Backjoon/src/Step02/_06.java
+++ Backjoon/src/Step02/_06.java
@@ -1,10 +1,34 @@
 package Step02;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2022/08/22
  * 이름 : 박종협
  * 내용 : 백준 2단계 6번 : 오븐 시계
  */
 public class _06 {
-
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		
+		if(m+n>60) {
+			if(h>23) {
+				System.out.print((h+1)-24);
+				System.out.println((m+n)-60);
+			}else {
+				System.out.print(h+1);
+				System.out.println((m+n)-60);
+			}
+		}else if(h==24) {
+			System.out.print(h-24);
+			System.out.println(m+n);
+		}else {
+			System.out.print(h);
+			System.out.println(m+n);
+		}
+	}
 }
