bfa92d4311f91dd0610f4b1ae342b04b5f92c074
lazca2080 <lazca2080@gmail.com>
2022-08-24 19:52:41 +0900

    0824

--- Backjoon/src/Step03/_07.java
+++ Backjoon/src/Step03/_07.java
@@ -2,11 +2,15 @@ package Step03;
 
 import java.util.Scanner;
 
+/*
+ * 날짜 : 2022/08/24
+ * 이름 : 박종협
+ * 내용 : 백준 3단계 7번 : A+B-8
+ */
 public class _07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		
 		
 		for(int i=1; i<=a; i++) {
 			int x = sc.nextInt();
