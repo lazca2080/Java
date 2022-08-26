d93919eef979462fdbbbb682da45d3f0121dd291
lazca2080 <lazca2080@gmail.com>
2022-08-26 14:55:55 +0900

    0826

--- PowerJava/src/ch04/P151_BoxTest.java
+++ PowerJava/src/ch04/P151_BoxTest.java
@@ -9,7 +9,6 @@ public class P151_BoxTest {
 		vol.length = 20;
 		vol.width = 30;
 		
-		
 		System.out.println("상자의 가로, 세로, 높이는 "+vol.height+" ,"+vol.length+", "+vol.width+"입니다.");
 		System.out.println("상자의 부피는 " + vol.volume() + "입니다.");
 		
