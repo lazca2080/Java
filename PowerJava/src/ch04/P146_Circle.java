5d388ac7cc385b74f4f992e1869538898e108fdd
lazca2080 <lazca2080@gmail.com>
2022-08-29 16:46:38 +0900

    0829

--- PowerJava/src/ch04/P146_Circle.java
+++ PowerJava/src/ch04/P146_Circle.java
@@ -2,10 +2,16 @@ package ch04;
 
 public class P146_Circle {
 		
-			public int radius;
-			public String color;
+			private int radius;
+			private String color;
+			
+			public P146_Circle(int radius, String color) {
+				this.radius = radius;
+				this.color  = color;
+			}
 			
 			public double getArea() {
 				return 3.14*radius*radius;
+				
 	}
 }
--- PowerJava/src/ch04/P147_CircleTest.java
+++ PowerJava/src/ch04/P147_CircleTest.java
@@ -8,16 +8,7 @@ public class P147_CircleTest {
 		//obj = new Circle();
 		
 		
-		P146_Circle obj = new P146_Circle();
-		
-		obj.radius = 100;
-		
-		obj.color = "blue"; // 객체의 필드 접근
-		
-		double area = obj.getArea(); // 객체 메소드 접근
-		String a = obj.color;
-		
-		System.out.println("원의 면적=" + area);
-		System.out.println("원의 색상=" + a);
+		P146_Circle obj = new P146_Circle(100, "Blue");
+		System.out.println(obj.getArea());
 	}
 }
