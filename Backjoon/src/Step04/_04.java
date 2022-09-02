fab1b1002018712016a359a56f5695f2c5478bbd
lazca9527 <lazca9527@gmail.com>
2022-09-02 11:49:35 +0900

    2022/09/02

--- Backjoon/src/Step04/_04.java
+++ Backjoon/src/Step04/_04.java
@@ -11,10 +11,11 @@ public class _04 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] arr = new int[N];
 		int sum = 0;
+		int[] arr = new int[N];
 		
 		for(int i = 0; i<N; i++) {
+			
 			int a = sc.nextInt();
 			arr[i] = a;
 			sum += arr[i];
--- Backjoon/src/Step04/_06.java
+++ Backjoon/src/Step04/_06.java
@@ -1,5 +1,40 @@
 package Step04;
 
-public class _06 {
+import java.util.Scanner;
 
+/*
+ * 날짜 : 2022/09/01
+ * 이름 : 박종협
+ * 내용 : 백준 4단계 6번 : 평균은 넘겠지
+ */
+public class _06 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		int sum = 0;
+		int avg = 0;
+		int count = 0;
+		
+		for(int x = 0; x<C; x++) {
+			int N = sc.nextInt();
+			int[] arr = new int[N];
+			for(int y = 0; y<N; y++) {
+				int i = sc.nextInt();
+				arr[y] = i;
+				sum += i;
+				
+			}
+			for(int a = 0; a<arr.length; a++) {
+				avg = sum/N;
+				if(avg < arr[a]) {
+					count ++;
+				} 
+			}
+			float a1 = (float)count/N*100;
+			System.out.println(String.format("%.3f", a1)+"%");
+			sum = 0;
+			avg = 0;
+			count = 0;
+		}
+	}
 }
