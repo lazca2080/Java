a3dd76269bf7d51b618da4d5588b6a24293645de
lazca2080 <lazca2080@gmail.com>
2022-09-08 19:46:19 +0900

    2022/09/08

--- Backjoon/src/Step05/_03.java
+++ Backjoon/src/Step05/_03.java
@@ -6,23 +6,14 @@ public class _03 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String S = sc.next();
-		int S1;
-		int a = S.length();
-		int count = 1;
-		int num = 0;
 		
-		for(int x = 0; x<a; x++) {
-			S1 = S.charAt(x);
-			for(int y = 0; y<=25; y++) {
-				if(y == S.charAt(x)-97) {
-					break;
-				}else {
-					count++;
-				}
-			}System.out.println(count);
-			
+		int y = S.indexOf("a");
 		
-		}		
+		if(y == -1) {
+			System.out.println(-1);
+		}else {
+			System.out.println(y);
+		}
 		
 	}
 }
