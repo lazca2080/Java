6947f6f7c5c660eb98b620ba737c48da271d9d52
lazca2080 <lazca2080@gmail.com>
2022-09-14 16:47:22 +0900

    2022/09/14

--- Ch10/src/sub4/PropertiesTest.java
+++ Ch10/src/sub4/PropertiesTest.java
@@ -1,11 +1,19 @@
 package sub4;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.Properties;
 
 /*
  * 날짜 : 2022/09/14
  * 이름 : 박종협
  * 내용 : 프로퍼티 실습하기
+ * 
+ * 프로퍼티(Properties)
+ * 	- 문자열 데이터만 취급하는 Key-Value 자료구조
+ * 	- 프로그램의 환경설정값을 저장할때 사용하는 자료구조
+ * 	- 입출력 스트림으로 데이터 입출력 
  */
 public class PropertiesTest {
 	
@@ -19,6 +27,36 @@ public class PropertiesTest {
 		prop.setProperty("C", "Cherry");
 		
 		System.out.println(prop);
+		
+		// 프로퍼티 파일 생성
+		String path ="C:\\Users\\java2\\Desktop\\Fruit.properties";
+		
+		try {
+			FileOutputStream fos = new FileOutputStream(path);
+			prop.store(fos, null); // 파일쓰기
+			
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("프로퍼티 파일 생성완료...");
+		
+		// 프로퍼티 객체 생성
+		String target ="C:\\Users\\java2\\Desktop\\Cities.properties";
+		Properties propCities = new Properties();
+		
+		try {
+			FileInputStream fis = new FileInputStream(target);
+			propCities.load(fis); //파일 읽기
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(propCities);
+		System.out.println(propCities.getProperty("Kor"));
+		System.out.println("프로퍼티 객체 생성완료...");
 		
 	}
 	
---
+++ Ch11/src/sub1/JDBCTest.java
@@ -0,0 +1,42 @@
+package sub1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/*
+ * 날짜 : 2022/09/14
+ * 이름 : 박종협
+ * 내용 : JDBC 실습
+ * 
+ * JDBC(Java DataBase Connectivity)
+ *  - 자바에서 데이터베이스 연결을 위한 기술표준
+ *  - Java는 기본적으로 JDBC 라이브러리 포함 해당 DBMS에 맞는 드라이버만 다운로드&설정
+ */
+public class JDBCTest {
+	public static void main(String[] args) {
+		
+		// DB정보
+		String host = "jdbc:mysql://127.0.0.1:3306/java2db";
+		String user = "root";
+		String pass = "1234";
+		
+		try {
+			// 1단계 - JDBC 드라이버 로드
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2단계 - 데이터베이스 접속
+			Connection conn = DriverManager.getConnection(host, user, pass);
+		
+			if(conn != null) {
+				System.out.println("데이터베이스 접속 성공!");
+			}else {
+				System.out.println("데이터베이스 접속 실패!");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("데이터베이스 접속 테스트 완료...");
+	}
+}
