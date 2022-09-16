af81e362d002906b7b97fdc8c9765f8dcb378951
lazca2080 <lazca2080@gmail.com>
2022-09-16 17:05:00 +0900

    2022/09/16

---
+++ Exam/src/sub04/Test07.java
@@ -0,0 +1,21 @@
+package sub04;
+
+/*
+ * 날짜 : 2022/09/16
+ * 이름 : 박종협
+ * 내용 : 리스트 연습문제
+ */
+public class Test07 { 
+	public static void main(String[] args) {
+		
+		// 60~100 사이 임의의 수 10개를 더하므로 결과값이 항상 다름
+		printList(createList());
+	}
+	
+	public static void printList() {
+		
+	}
+	
+	public static 
+
+}
---
+++ Exam/src/sub04/Test09.java
@@ -0,0 +1,31 @@
+package sub04;
+
+/*
+ * 날짜 : 2022/09/16
+ * 이름 : 박종협
+ * 내용 : 객체 리스트 연습문제
+ */
+class Member {
+	String id;
+	String name;
+	int age;
+	
+	Member(String id, String name, int age){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
+	@Override
+	public String Member() {
+		
+	}
+	
+	
+}
+
+
+public class Test09 {
+	
+
+}
